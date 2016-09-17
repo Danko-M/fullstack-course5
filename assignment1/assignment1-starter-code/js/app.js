5a87b972332924c7bad521ad9820f9981349a179
danko-m <dankomilutinovic@gmail.com>
2016-09-17 19:02:45 +0200

    improvements

--- assignment1/assignment1-starter-code/js/app.js
+++ assignment1/assignment1-starter-code/js/app.js
@@ -7,12 +7,11 @@ angular.module('MsgApp', [])
 MsgController.$inject = ['$scope'];
 function MsgController($scope) {
   $scope.message = "";
-
   $scope.items = "";
-
 
   $scope.feedYaakov = function () {
     var commas = $scope.items.split(",").length || 0;
+
     if ($scope.items.length === 0) {
         $scope.message = "Please enter data first"; 
     }
