d5698507fd9c2f279c78536de82cbb208a5f4408
danko-m <dankomilutinovic@gmail.com>
2016-09-17 18:51:35 +0200

    added code for a first assignment

---
+++ assignment1/assignment1-starter-code/js/app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.message = "";
+
+  $scope.items = "";
+
+
+  $scope.feedYaakov = function () {
+    var commas = $scope.items.split(",").length || 0;
+    if ($scope.items.length === 0) {
+        $scope.message = "Please enter data first"; 
+    }
+     else if (commas <= 3){
+      $scope.message =  "Enjoy!";
+    } else if (commas > 3){
+      $scope.message =  "To much!";
+    }
+  };
+}
+
+})();
