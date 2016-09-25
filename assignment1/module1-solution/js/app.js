ac1803cbae0c6fd19164eab5e35bd606d794ffc9
danko-m <dankomilutinovic@gmail.com>
2016-09-25 17:21:51 +0200

    assignment2 init commit

--- assignment1/module1-solution/js/app.js
+++ assignment1/module1-solution/js/app.js
@@ -14,16 +14,16 @@ function LunchCheckController($scope) {
 
     if ($scope.items.length === 0) {
       $scope.message = "Please enter data first";
-      $scope.color = "color:#ff0000";
-      $scope.borderColor = "border-color:#ff0000"; 
+      $scope.color = "color:#f00";
+      $scope.borderColor = "border-color:#f00"; 
     } else if (commas <= 3){
       $scope.message = "Enjoy!";
-      $scope.color = "color:#00ff00";
-      $scope.borderColor = "border-color:#00ff00";
+      $scope.color = "color:#0f0";
+      $scope.borderColor = "border-color:#0f0";
     } else if (commas > 3){
       $scope.message = "Too much!";
-      $scope.color = "color:#00ff00";
-      $scope.borderColor = "border-color:#00ff00";
+      $scope.color = "color:#0f0";
+      $scope.borderColor = "border-color:#0f0";
     }
   };
 
